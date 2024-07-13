0a76cb658411d0cd3b9d2902789bb3ec4ed758a7
Emerson Connelly <emerconn7@gmail.com>
2024-07-12 22:21:09 -0500

    cilium stuff

---
+++ kubernetes/flux/infra/cni/cilium.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+helm install \
+    cilium \
+    cilium/cilium \
+    --version 1.15.7 \
+    --namespace kube-system \
+    --set ipam.mode=kubernetes \
+    --set kubeProxyReplacement=true \
+    --set securityContext.capabilities.ciliumAgent="{CHOWN,KILL,NET_ADMIN,NET_RAW,IPC_LOCK,SYS_ADMIN,SYS_RESOURCE,DAC_OVERRIDE,FOWNER,SETGID,SETUID}" \
+    --set securityContext.capabilities.cleanCiliumState="{NET_ADMIN,SYS_ADMIN,SYS_RESOURCE}" \
+    --set cgroup.autoMount.enabled=false \
+    --set cgroup.hostRoot=/sys/fs/cgroup \
+    --set k8sServiceHost=localhost \
+    --set k8sServicePort=7445
+
+helm upgrade cilium cilium/cilium --version 1.15.7 \
+   --namespace kube-system \
+   --reuse-values \
+   --set l2announcements.enabled=true \
+   --set k8sClientRateLimit.qps={QPS} \
+   --set k8sClientRateLimit.burst={BURST} \
+   --set kubeProxyReplacement=true
