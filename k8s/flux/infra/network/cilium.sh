3548aa7677aa00a30900a1aa6668f547ea5e2c0a
Emerson Connelly <emerconn7@gmail.com>
2024-09-23 18:38:34 -0500

    enable Cilium traces

--- k8s/flux/infra/network/cilium.sh
+++ k8s/flux/infra/network/cilium.sh
@@ -21,11 +21,14 @@ helm upgrade \
     --set prometheus.serviceMonitor.enabled=true \
     --set operator.prometheus.enabled=true \
     --set operator.prometheus.serviceMonitor.enabled=true \
-    --set hubble.relay.enabled=true \
+    --set hubble.enabled=true \
     --set hubble.ui.enabled=true \
+    --set hubble.relay.enabled=true \
+    --set hubble.metrics.enableOpenMetrics=true \
+    --set hubble.metrics.enabled="{dns,drop,tcp,flow,port-distribution,icmp,httpV2:exemplars=true;labelsContext=source_ip\,source_namespace\,source_workload\,destination_ip\,destination_namespace\,destination_workload\,traffic_direction}" \
+    --set ingressController.enabled=true \
+    --set gatewayAPI.enabled=true \
     --set l2announcements.enabled=true \
     --set l2announcements.leaseDuration="3s" \
     --set l2announcements.leaseRenewDeadline="1s" \
-    --set l2announcements.leaseRetryPeriod="500ms" \
-    --set ingressController.enabled=true \
-    --set gatewayAPI.enabled=true
+    --set l2announcements.leaseRetryPeriod="500ms"
