fbddd4f9203b53cec6c27eacf71bc713b32336f8
Emerson Connelly <emerconn7@gmail.com>
2025-11-24 19:50:19 -0600

    less words

--- k8s/talos/cilium/deploy.sh
+++ k8s/talos/cilium/deploy.sh
@@ -4,5 +4,5 @@ helm repo add cilium https://helm.cilium.io/
 helm repo update
 
 helm upgrade --install cilium cilium/cilium \
-    --values ../../flux/clusters/tal-clu-1/infra/network/cilium/helm-release-values.yaml \
+    --values ../../flux/clusters/tal-clu-1/infra/network/cilium/helm-values.yaml \
     --namespace kube-system
