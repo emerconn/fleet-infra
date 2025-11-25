db43a494b2f2c10e2a56e53f80d31d5324549247
Emerson Connelly <emerconn7@gmail.com>
2025-11-24 19:49:13 -0600

    use one helm values file

--- k8s/talos/cilium/deploy.sh
+++ k8s/talos/cilium/deploy.sh
@@ -4,5 +4,5 @@ helm repo add cilium https://helm.cilium.io/
 helm repo update
 
 helm upgrade --install cilium cilium/cilium \
-    --values values.yaml \
+    --values ../../flux/clusters/tal-clu-1/infra/network/cilium/helm-release-values.yaml \
     --namespace kube-system
